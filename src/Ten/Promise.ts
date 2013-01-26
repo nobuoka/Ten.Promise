1b5d9938a801fd62b1e17e02c5b4f256849c0329
nobuoka <nobuoka@vividcode.info>
2013-01-26 21:02:20 +0900

    とりあえず通常の値を返す場合の正常系

--- src/Ten/Promise.ts
+++ src/Ten/Promise.ts
@@ -1,20 +1,67 @@
 module Ten {
-    export class Promise {
-        private __onCancel;
-        constructor(init, onCancel) {
-            this.__onCancel = onCancel;
-            function comp(val) {
-            }
-            function err(val) {
-            }
-            function prog(val) {
-            }
-            try {
-                init(comp, err, prog);
-            } catch (err) {
+    interface PromiseListener {
+        promise?: AbstractPromise;
+        s: (val) => any;
+    }
+    export class AbstractPromise {
+        private __sucVal;
+        private __listeners: PromiseListener[];
+        constructor () {
+            this.__listeners = [];
+        }
+        private __notifySuccess() {
+            var val = this.__sucVal;
+            var listeners = this.__listeners;
+            while (listeners.length > 0) {
+                var listener = listeners.shift();
+                this.__notifyListenerOfSuccess(listener, val);
             }
         }
+        _setValue(val) {
+            this.__sucVal = val;
+            this.__notifySuccess();
+        }
         cancel() {
+        }
+        private __notifyListenerOfSuccess(listener: PromiseListener, value: any) {
+            // value or Promise, or exception
+            var callbackRes;
+            try {
+                callbackRes = listener.s(this.__sucVal);
+            } catch (err) {
+                callbackRes = err;
+            }
+            // 普通の値だったら
+            listener.promise._setValue(callbackRes);
+        }
+        private __registerListener(listener: PromiseListener) {
+            if (this.__sucVal) {
+                this.__notifyListenerOfSuccess(listener, this.__sucVal);
+            } else {
+                this.__listeners.push(listener);
+            }
+        }
+        then(onSuccess: (val) => Promise): AbstractPromise {
+            var p = new SimplePromise();
+            this.__registerListener({ s: onSuccess, promise: p });
+            return p;
+        }
+    }
+    class SimplePromise extends AbstractPromise {}
+    export class Promise extends AbstractPromise {
+        private __onCancel;
+        constructor(init, onCancel) {
+            super();
+            this.__onCancel = onCancel;
+            var that = this;
+            function comp(val) {
+                that._setValue(val);
+            }
+            try {
+                init(comp);
+            } catch (err) {
+                // ここどうしよう
+            }
         }
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,39 @@
 module("Ten.Promise");
 
-test("Ten.Promise の普通の使い方", function () {
-    var p = new Ten.Promise();
+asyncTest("Ten.Promise を同期呼び出しで使う", function () {
+    var p = new Ten.Promise(function (suc) {
+        suc(100);
+    });
     ok(p, "値がある");
+
+    p.then(function (val) {
+        equal(val, 100);
+        return 120;
+    }).then(function (val) {
+        equal(val, 120);
+        return "文字列を返す";
+    }).then(function (val) {
+        equal(val, "文字列を返す");
+        QUnit.start();
+    });
+});
+
+asyncTest("Ten.Promise を非同期呼び出しで使う", function () {
+    var p = new Ten.Promise(function (suc) {
+        setTimeout(function () {
+            suc(100);
+        }, 200);
+    });
+    ok(p, "値がある");
+
+    p.then(function (val) {
+        equal(val, 100);
+        return 120;
+    }).then(function (val) {
+        equal(val, 120);
+        return "文字列を返す";
+    }).then(function (val) {
+        equal(val, "文字列を返す");
+        QUnit.start();
+    });
 });
